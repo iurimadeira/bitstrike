aedbb20cdaf474fc73969d3fc3d8c7ad064c8f1b
Iuri Madeira <iurimadeira@gmail.com>
2015-04-15 15:27:04 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection", function(socket){
+	var username;
+
+	socket.on("user-connected", function(name){
+		username = name;
+		console.log(name + " joined the game.");
+		io.emit("user-connected", username);
+	});
+
+	socket.on("chat-message", function(msg){
+		console.log("message: " + msg);
+		io.emit("chat-message", msg);
+	});
+
+	socket.on("disconnect", function(socket){
+		console.log(username + " has left the game.");
+		io.emit("user-disconnected", username);
+	});
+});
+
+http.listen(3000, function(){
+	console.log("Listening on " + 3000);
+});
